ac19339dfc5f5124e487dd30cd704887f79c3026
2minshaorui <69134066+2minshaorui@users.noreply.github.com>
2021-02-16 14:00:35 +0800

    Added edits for issues 32 and 33

--- jinder/src/main/java/sg/edu/iss/jinder/repo/JobRepository.java
+++ jinder/src/main/java/sg/edu/iss/jinder/repo/JobRepository.java
@@ -1,8 +1,9 @@
 package sg.edu.iss.jinder.repo;
 
-import java.util.List; 
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import sg.edu.iss.jinder.model.Job;
@@ -13,5 +14,8 @@ public interface JobRepository extends JpaRepository<Job, Integer> {
 	public List<Job> findAll();
 	
 	public Job findById(int id);
+	
+	@Query("SELECT j FROM Job j ORDER BY j.companyName")
+	List<Job> findAllSortedByAsc();
 
 }
